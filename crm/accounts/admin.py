af29063cc9ee9eaad52e79b576932cfaee02fa17
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-03-18 23:48:41 +0000

    Added Tag class to models.py

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import *
+from .models import Customer, Product, Tag, Order
 
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 admin.site.register(Order)
