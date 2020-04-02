b2242b4fcc1a56c5d7c21a6aa626eb8a8ce86d5e
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-04-02 23:02:58 +0100

    dded admin_only function to decorators.py

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -5,10 +5,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 from .models import Customer, Product, Tag, Order
 from .forms import OrderForm, CreateUserForm
 from .filters import OrderFilter
-from .decorators import unauthenticated_user, allowed_users 
+from .decorators import unauthenticated_user, allowed_users, admin_only
 
 # Create your views here.
 
@@ -19,9 +20,13 @@ def registerPage(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+            
+            messages.success(request, 'Account was created for ' + username)
             return redirect('login')
 
     context = {'form':form}
@@ -52,7 +57,7 @@ def logoutUser(request):
 
 
 @login_required(login_url='login')
-@allowed_users(allowed_roles=['admin'])
+@admin_only
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
