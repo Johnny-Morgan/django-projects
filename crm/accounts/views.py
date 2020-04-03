f15efcd9136726989d80d8d05f818d40c1b7c162
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-04-03 22:28:44 +0100

    user.html updates

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -25,7 +25,7 @@ def registerPage(request):
 
             group = Group.objects.get(name='customer')
             user.groups.add(group)
-            
+            Customer.objects.create(user=user)
             messages.success(request, 'Account was created for ' + username)
             return redirect('login')
 
@@ -72,12 +72,20 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count
+    context = {'orders':orders,'total_orders':total_orders, 
+    'delivered':delivered, 'pending':pending}
     return render(request, 'accounts/user.html', context)
 
 
 @login_required(login_url='login')
+@allowed_users(allowed_roles=['admin'])
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products':products})
