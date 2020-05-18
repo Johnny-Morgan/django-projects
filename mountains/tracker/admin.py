640b91824a4aaad7eeb2bedf48519b6e5fe62f52
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-05-18 23:51:59 +0100

    Set up database

--- mountains/tracker/admin.py
+++ mountains/tracker/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Mountain
+
+admin.site.register(Mountain)
--- mountains/tracker/models.py
+++ mountains/tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Mountain(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    height = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+    area = models.CharField(max_length=200, null=True)
+    date_climbed = models.DateField(null=True)
+
+    def __str__(self):
+        return self.name
+
