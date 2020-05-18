3d144c845dc735b46aeca5334b832f5c49bc9d38
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-05-19 00:49:30 +0100

    models.py updates
    Added Hike class
    Added dropdown to Mountain class

--- mountains/tracker/models.py
+++ mountains/tracker/models.py
@@ -1,11 +1,26 @@
 from django.db import models
 
 class Mountain(models.Model):
+    AREAS = (
+            ('Dublin/Wicklow', 'Dublin/Wicklow'),
+            ('North Midlands', 'North Midlands'),
+            ('East Coast', 'East Coast'),
+            ('Snowdonia', 'Snowdonia')
+    )
     name = models.CharField(max_length=200, null=True)
-    height = models.DecimalField(max_digits=5, decimal_places=2, null=True)
-    area = models.CharField(max_length=200, null=True)
+    height = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    area = models.CharField(max_length=200, null=True, choices=AREAS)
     date_climbed = models.DateField(null=True)
 
     def __str__(self):
         return self.name
 
+class Hike(models.Model):
+    length = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    duration = models.CharField('Duration (hh:mm:ss)',max_length=200, null=True)
+    total_ascent = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    total_descent = models.DecimalField(max_digits=6, decimal_places=2, null=True)
+    notes = models.TextField(null=True)
+
+    def __str__(self):
+        return f'{self.length} km'
