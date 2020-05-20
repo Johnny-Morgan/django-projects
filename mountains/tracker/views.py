9981f6e8caa942f734fb956dd9ccfa3eaea03a5c
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-05-20 01:07:25 +0100

    Display mountains and hikes

--- mountains/tracker/views.py
+++ mountains/tracker/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Mountain, Hike
 
 def home(request):
-    return render(request, 'tracker/dashboard.html')
+    last_5_mountains = Mountain.objects.all().order_by('-date_climbed')[:5]
+    last_5_hikes = Hike.objects.all().order_by('-hike_date')[:5]
+    context = {'last_5_mountains': last_5_mountains, 'last_5_hikes': last_5_hikes}
+    return render(request, 'tracker/dashboard.html', context)
 
 def mountains(request):
-    return render(request, 'tracker/mountains.html')
+    mountains = Mountain.objects.all() 
+    context = {'mountains': mountains}
+    return render(request, 'tracker/mountains.html', context)
 
 def hikes(request):
-    return render(request, 'tracker/hikes.html')
+    hikes = Hike.objects.all()
+    context = {'hikes': hikes}
+    return render(request, 'tracker/hikes.html', context)
