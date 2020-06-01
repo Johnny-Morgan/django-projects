69b7643c9f9370be1619bc0d89730822bdcad7a1
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-06-01 23:32:26 +0100

    Add Image class

--- mountains/tracker/models.py
+++ mountains/tracker/models.py
@@ -9,7 +9,7 @@ class Mountain(models.Model):
     )
     name = models.CharField(max_length=200, null=True)
     height = models.DecimalField(max_digits=6, decimal_places=2, null=True)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True) 
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     area = models.CharField(max_length=200, null=True, choices=AREAS)
     date_climbed = models.DateField(null=True)
@@ -26,6 +26,29 @@ class Hike(models.Model):
     total_descent = models.DecimalField('Total Decsent (m)', max_digits=6, decimal_places=2, null=True)
     hike_date = models.DateField(null=True)
     notes = models.TextField(null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.length} km'
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.featured_image.url
+        except:
+            url=''
+        return url
+
+class Image(models.Model):
+    hike = models.ForeignKey(Hike, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url=''
+        return url
+
+
