89cbe72bccd2fff1b9da87b8d3b98f041bb8d46e
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-07-01 00:00:49 +0100

    Add datepicker to forms.py

--- mountains/tracker/forms.py
+++ mountains/tracker/forms.py
@@ -1,14 +1,22 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Mountain, Hike
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class MountainForm(ModelForm):
     class Meta:
         model = Mountain
         fields = '__all__'
-
+        widgets = {
+            'date_climbed': DateInput(),
+        }
 
 class HikeForm(ModelForm):
     class Meta:
         model = Hike
         fields = '__all__'
+        widgets = {
+            'hike_date': DateInput(),
+        }
