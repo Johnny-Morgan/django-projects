e38c0a5c7c46774df153ac658ea19b0c3f57ff74
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-07-10 23:43:58 +0100

    Update registerPage function

--- mountains/tracker/views.py
+++ mountains/tracker/views.py
@@ -8,6 +8,10 @@ from .filters import MountainFilter, HikeFilter
 
 def registerPage(request):
     form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
     context = {'form': form}
     return render(request, 'tracker/register.html', context)
 
