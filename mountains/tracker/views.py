99010d216e27b166482ac3a6dd0b4efeacd399a4
John Morgan <37510013+Johnny-Morgan@users.noreply.github.com>
2020-07-28 23:44:19 +0100

    Update registerPage & loginPage functions

--- mountains/tracker/views.py
+++ mountains/tracker/views.py
@@ -10,31 +10,37 @@ from .forms import MountainForm, HikeForm, CreateUserForm
 from .filters import MountainFilter, HikeFilter
 
 def registerPage(request):
-    form = CreateUserForm()
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
-            return redirect('login')
-    context = {'form': form}
-    return render(request, 'tracker/register.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else: 
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'tracker/register.html', context)
 
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else: 
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR password is incorrect')
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR password is incorrect')
 
-    context = {}
-    return render(request, 'tracker/login.html', context)
+        context = {}
+        return render(request, 'tracker/login.html', context)
 
 def logoutUser(request):
     logout(request)
